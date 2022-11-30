b35fabd758efe4bb018e7e86b5eee37802419d50
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-11-30 16:51:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int n =0,e=2;
+    bool f;
+    cin >> n;
+    f = primo(n,e);
+    if (f)
+    {
+        cout<<"numero primo";
+    }
+    else
+    {
+        cout<<"numero non primo";
+    }
+    return 0;
 }
