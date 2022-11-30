39007af03bcb2dd9226bdea9a4da8cde5b90f029
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-11-30 16:52:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
-
+#include "lib.h"
+bool primo(int a, int c)
+{
+    if (a==0||a==1)
+    {
+        return true;
+    }
+    else if(a%c==0){
+        return a==c;
+    }
+    else
+    {
+        primo(a,c+1);
+    }
+}
